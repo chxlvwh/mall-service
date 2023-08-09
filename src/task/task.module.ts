72fdbb7017a425ec4836b5e96237b640c795cd01
lenny.cao <lenny.cao@h8tch.com>
2023-08-09 23:52:00 +0800

    feat: 添加任务

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { OrderSetting } from '../order-setting/order-setting.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from '../order/entity/order.entity';
 
 @Module({
-	imports: [ScheduleModule.forRoot()],
+	imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([OrderSetting, Order])],
 	controllers: [TaskController],
 	providers: [TaskService],
 })
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { OrderSetting, OrderSettingKey } from '../order-setting/order-setting.entity';
+import { Order, OrderStatus } from '../order/entity/order.entity';
 
 @Injectable()
 export class TaskService {
-	@Cron(CronExpression.EVERY_MINUTE)
-	handleUnpaidOrderTimeout() {
+	constructor(
+		@InjectRepository(OrderSetting)
+		private readonly orderSettingRepository: Repository<OrderSetting>,
+		@InjectRepository(Order)
+		private readonly orderRepository: Repository<Order>,
+	) {}
+	@Cron(CronExpression.EVERY_5_SECONDS)
+	async handleUnpaidOrderTimeout() {
 		console.log('每分钟执行一次');
+		const normalOrderOverTime = await this.orderSettingRepository.findOne({
+			where: { key: OrderSettingKey.NormalOrderTimeout },
+		});
+		const unpaidOrders = await this.orderRepository.find({
+			where: { status: OrderStatus.UNPAID },
+		});
 	}
 }

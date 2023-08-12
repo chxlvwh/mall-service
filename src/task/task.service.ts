7a1f04102c9fe95218b10b337a84167d8634bd65
lenny.cao <lenny.cao@h8tch.com>
2023-08-12 10:44:59 +0800

    feat: 正常订单超时关闭

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { OrderSetting, OrderSettingKey } from '../order-setting/order-setting.entity';
 import { Order, OrderStatus } from '../order/entity/order.entity';
+import { log } from 'winston';
 
 @Injectable()
 export class TaskService {
@@ -13,14 +14,28 @@ export class TaskService {
 		@InjectRepository(Order)
 		private readonly orderRepository: Repository<Order>,
 	) {}
-	@Cron(CronExpression.EVERY_5_SECONDS)
+	@Cron(CronExpression.EVERY_MINUTE)
 	async handleUnpaidOrderTimeout() {
 		console.log('每分钟执行一次');
+		/** 1. 获取订单超时时间 */
 		const normalOrderOverTime = await this.orderSettingRepository.findOne({
 			where: { key: OrderSettingKey.NormalOrderTimeout },
 		});
+		/** 2. 获取未支付订单 */
 		const unpaidOrders = await this.orderRepository.find({
 			where: { status: OrderStatus.UNPAID },
 		});
+		/** 3. 判断是否超时 */
+		for (let i = 0; i < unpaidOrders.length; i++) {
+			const order = unpaidOrders[i];
+			const now = new Date();
+			const orderTime = order.createdAt;
+			const orderOverTime = new Date(orderTime.getTime() + normalOrderOverTime.value * 60 * 1000);
+			if (now > orderOverTime) {
+				/** 4. 超时则关闭订单 */
+				order.status = OrderStatus.CLOSED;
+				await this.orderRepository.save(order);
+			}
+		}
 	}
 }
